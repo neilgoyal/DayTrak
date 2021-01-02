e3f160f21adb9e1df142e8f6e6dc2075d5d0cb4f
Neil Goyal <neilgo1234@gmail.com>
2021-01-02 23:08:50 +0530

    appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,43 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-// class _HomePageState extends State<HomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         home: Scaffold(
-//             appBar: AppBar(
-//                 backgroundColor: Colors.white,
-//                 elevation: 0,
-//                 title: Text(
-//                   "Date",
-//                   style: TextStyle(
-//                     fontFamily: 'Protipo Compact',
-//                     fontSize: 40,
-//                     color: const Color(0xff9b8fb1),
-//                     fontWeight: FontWeight.w100,
-//                   ),
-//                   textAlign: TextAlign.left,
-//                 )
-//               )
-//             )
-//           );
-//   }
-// }
+var dt = DateTime.now();
+String newDt = DateFormat.MMMd().format(dt);
 
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
             title: Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [Text("date")],
-    )));
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  newDt,
+                  style: TextStyle(
+                    fontFamily: 'Protipo Compact',
+                    fontSize: 40,
+                    color: const Color(0xff9b8fb1),
+                    fontWeight: FontWeight.w100,
+                  ),
+                ),
+                Text("Day 1",
+                    style: TextStyle(
+                      fontFamily: 'Protipo Compact',
+                      fontSize: 40,
+                      color: const Color(0xff9b8fb1),
+                      fontWeight: FontWeight.w100,
+                    ))
+              ],
+            )));
   }
 }
