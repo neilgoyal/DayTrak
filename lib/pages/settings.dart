e132427aa95e20c49f5a6a25faff6c9f1bc63720
Vinayak Kapur <kapurvinayak2004@gmail.com>
2021-01-02 21:15:11 +0530

    Co-authored-by: neil goyal <neilgo1234@gmail.com>

---
+++ lib/pages/settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+}
---
+++ lib/pages/timetable.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TimetablePage extends StatefulWidget {
+  @override
+  _TimetableState createState() => _TimetableState();
+}
+
+class _TimetableState extends State<TimetablePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
