795ef9705a4b3eceaa5d626b8a76f1d33f14c2d8
Vinayak Kapur <kapurvinayak2004@gmail.com>
2021-01-07 03:45:53 +0530

    yessir

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:weather/weather.dart';
-import 'dart:async';
 import 'package:schoolcalendar/addtask.dart';
-
-List<Weather> _data = [];
 
 class HomePage extends StatefulWidget {
   @override
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -4,9 +4,9 @@ import 'dart:convert';
 import 'package:schoolcalendar/weather.dart';
 import 'package:http/http.dart' as http;
 
-Future<WeatherModel> getWeather(String lat, String lng) async {
+Future<WeatherModel> getWeather() async {
   final response = await http.get(
-      'http://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lng&appid=77580a3797c4f2efd008403c9faf5e22&units=metric');
+      'https://api.openweathermap.org/data/2.5/weather?lat=28.4667&lon=77.0333&appid=77580a3797c4f2efd008403c9faf5e22&units=metric');
 
   if (response.statusCode == 200) {
     var result = json.decode(response.body);
@@ -25,28 +25,32 @@ class _SettingsState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tempyurtatur'),
-      ),
       body: Container(
-        decoration: BoxDecoration(color: Colors.white),
-        child: Center(
-          child: FutureBuilder<WeatherModel>(
-            future: getWeather('77.0333', '28.4667'),
-            builder: (context, snapshot) {
-              if (snapshot.hasData)
-                return Text(
-                  '${snapshot.data.name}',
-                  style: TextStyle(fontSize: 30),
-                );
-              else if (snapshot.hasError)
-                return Text(
-                  '${snapshot.error}',
-                  style: TextStyle(fontSize: 30, color: Colors.red),
-                );
-              return CircularProgressIndicator();
-            },
-          ),
+        child: FutureBuilder<WeatherModel>(
+          future: getWeather(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              WeatherModel model = snapshot.data;
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    '${model.main.temp.round()} ˚C',
+                    style:
+                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  )
+                ],
+              );
+            } else if (snapshot.hasError)
+              return Text(
+                '${snapshot.error}',
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.red,
+                    fontFamily: 'Protipo Compact'),
+              );
+            return CircularProgressIndicator();
+          },
         ),
       ),
     );
