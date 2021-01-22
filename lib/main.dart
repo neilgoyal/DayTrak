6ceec52b945796a19070262347c9769530c71dd5
Vinayak Kapur <kapurvinayak2004@gmail.com>
2021-01-23 00:04:02 +0530

    n

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:bubbled_navigation_bar/bubbled_navigation_bar.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/rendering.dart';
-import 'pages/home.dart';
-import 'pages/settings.dart';
-import 'pages/timetable.dart';
+import 'package:custom_navigation_bar/custom_navigation_bar.dart';
+import 'package:schoolcalendar/addtask1.dart';
+import 'package:schoolcalendar/pages/home.dart';
+import 'package:schoolcalendar/pages/settings.dart';
+import 'package:schoolcalendar/pages/timetable.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'School planner',
+      title: 'Custom Navigation Bar Example',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: MyHomePage(title: 'Example'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-  final titles = ['Home', 'TimeTable', 'Settings'];
-  final colors = [
-    Colors.black,
-    Colors.black,
-    Colors.black,
-  ];
-  final icons = [Icons.home_filled, Icons.access_time, Icons.settings];
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  PageController _pageController;
-  MenuPositionController _menuPositionController;
-  bool userPageDragging = false;
-
-  @override
-  void initState() {
-    _menuPositionController = MenuPositionController(initPosition: 0);
-    _pageController =
-        PageController(initialPage: 0, keepPage: false, viewportFraction: 1.0);
-    _pageController.addListener(handlePageChange);
-    super.initState();
-  }
-
-  void handlePageChange() {
-    _menuPositionController.absolutePosition = _pageController.page;
-  }
-
-  void checkUserDragging(ScrollNotification scrollNotification) {
-    if (scrollNotification is UserScrollNotification &&
-        scrollNotification.direction != ScrollDirection.idle) {
-      userPageDragging = true;
-    } else if (scrollNotification is ScrollEndNotification) {
-      userPageDragging = false;
-    }
-    if (userPageDragging) {
-      _menuPositionController.findNearestTarget(_pageController.page);
-    }
-  }
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: NotificationListener<ScrollNotification>(
-          // ignore: missing_return
-          onNotification: (scrollNotification) {
-            checkUserDragging(scrollNotification);
-          },
-          child: PageView(
-            controller: _pageController,
-            children: <Widget>[
-              HomePage(),
-              TimetablePage(),
-              SettingsPage(),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BubbledNavigationBar(
-          animationCurve: Curves.bounceIn,
-          animationDuration: Duration(milliseconds: 800),
-          controller: _menuPositionController,
-          initialIndex: 0,
-          itemMargin: EdgeInsets.symmetric(horizontal: 8),
-          backgroundColor: Color.fromRGBO(230, 230, 230, 1),
-          defaultBubbleColor: Color.fromRGBO(224, 163, 160, 1),
-          onTap: (index) {
-            _pageController.animateToPage(index,
-                curve: Curves.easeInOutQuad,
-                duration: Duration(milliseconds: 800));
-          },
-          items: widget.titles.map((title) {
-            var index = widget.titles.indexOf(title);
-            var color = widget.colors[index];
-            return BubbledNavigationBarItem(
-              icon: getIcon(index, color),
-              activeIcon: getIcon(index, Colors.white),
-              title: Text(
-                title,
-                style: TextStyle(color: Colors.white, fontSize: 12),
-              ),
-            );
-          }).toList(),
-        ));
+      // extendBody for floating bar get better perfomance
+      extendBody: true,
+      backgroundColor: Colors.white,
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
   }
 
-  Padding getIcon(int index, Color color) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 3),
-      child: Icon(widget.icons[index], size: 33, color: color),
+  Widget _buildBottomNavigationBar() {
+    return _buildBorderRadiusDesign();
+  }
+
+  Widget _buildBorderRadiusDesign() {
+    return CustomNavigationBar(
+      isFloating: true,
+      scaleFactor: 0.3,
+      iconSize: 30.0,
+      elevation: 3,
+      selectedColor: Colors.amber,
+      strokeColor: Colors.white,
+      unSelectedColor: Color(0xff6c788a),
+      backgroundColor: Colors.white,
+      borderRadius: Radius.circular(17),
+      items: [
+        CustomNavigationBarItem(
+          icon: Icon(Icons.home_filled),
+        ),
+        CustomNavigationBarItem(
+          icon: Icon(Icons.access_time),
+        ),
+        CustomNavigationBarItem(
+          icon: Icon(Icons.settings),
+        ),
+      ],
+      currentIndex: _currentIndex,
+      onTap: (index) {
+        setState(() {
+          _currentIndex = index;
+         
+        });
+      },
     );
   }
 }
