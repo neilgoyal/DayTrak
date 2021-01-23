aae5b00d35fcf2e2c27de3e6aa355a1cb5edfd2e
Neil Goyal <neilgo1234@gmail.com>
2021-01-23 23:21:02 +0530

    stable bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:custom_navigation_bar/custom_navigation_bar.dart';
-import 'package:schoolcalendar/pages/home.dart';
-import 'package:schoolcalendar/pages/settings.dart';
-import 'package:schoolcalendar/pages/timetable.dart';
+import 'package:bubbled_navigation_bar/bubbled_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
+import 'pages/home.dart';
+import 'pages/settings.dart';
+import 'pages/timetable.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'School planner',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -21,81 +24,96 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
+  final titles = ['Home', 'TimeTable', 'Settings'];
+  final colors = [
+    Colors.black,
+    Colors.black,
+    Colors.black,
+  ];
+  final icons = [Icons.home_filled, Icons.access_time, Icons.settings];
+  @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  int _currentIndex = 0;
-  TabController _tabController;
+class _MyHomePageState extends State<MyHomePage> {
+  PageController _pageController;
+  MenuPositionController _menuPositionController;
+  bool userPageDragging = false;
 
   @override
   void initState() {
+    _menuPositionController = MenuPositionController(initPosition: 0);
+    _pageController =
+        PageController(initialPage: 0, keepPage: false, viewportFraction: 1.0);
+    _pageController.addListener(handlePageChange);
     super.initState();
-    _tabController = new TabController(initialIndex: 0, vsync: this, length: 3);
-    _tabController.addListener(() {
-      setState(() {
-        _currentIndex = _tabController.index;
-      });
-    });
   }
 
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
+  void handlePageChange() {
+    _menuPositionController.absolutePosition = _pageController.page;
+  }
+
+  void checkUserDragging(ScrollNotification scrollNotification) {
+    if (scrollNotification is UserScrollNotification &&
+        scrollNotification.direction != ScrollDirection.idle) {
+      userPageDragging = true;
+    } else if (scrollNotification is ScrollEndNotification) {
+      userPageDragging = false;
+    }
+    if (userPageDragging) {
+      _menuPositionController.findNearestTarget(_pageController.page);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          HomePage(),
-          TimetablePage(),
-          SettingsPage(),
-        ],
-      ),
-      bottomNavigationBar: _buildBottomNavigationBar(),
-    );
+        body: NotificationListener<ScrollNotification>(
+          // ignore: missing_return
+          onNotification: (scrollNotification) {
+            checkUserDragging(scrollNotification);
+          },
+          child: PageView(
+            controller: _pageController,
+            children: <Widget>[
+              HomePage(),
+              TimetablePage(),
+              SettingsPage(),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BubbledNavigationBar(
+          animationCurve: Curves.bounceIn,
+          animationDuration: Duration(milliseconds: 800),
+          controller: _menuPositionController,
+          initialIndex: 0,
+          itemMargin: EdgeInsets.symmetric(horizontal: 8),
+          backgroundColor: Color.fromRGBO(230, 230, 230, 1),
+          defaultBubbleColor: Color.fromRGBO(224, 163, 160, 1),
+          onTap: (index) {
+            _pageController.animateToPage(index,
+                curve: Curves.easeInOutQuad,
+                duration: Duration(milliseconds: 800));
+          },
+          items: widget.titles.map((title) {
+            var index = widget.titles.indexOf(title);
+            var color = widget.colors[index];
+            return BubbledNavigationBarItem(
+              icon: getIcon(index, color),
+              activeIcon: getIcon(index, Colors.white),
+              title: Text(
+                title,
+                style: TextStyle(color: Colors.white, fontSize: 12),
+              ),
+            );
+          }).toList(),
+        ));
   }
 
-  Widget _buildBottomNavigationBar() {
-    return _buildBorderRadiusDesign();
-  }
-
-  Widget _buildBorderRadiusDesign() {
-    return CustomNavigationBar(
-      isFloating: false,
-      scaleFactor: 0.3,
-      iconSize: 30.0,
-      elevation: 3,
-      selectedColor: Colors.amber,
-      strokeColor: Colors.white,
-      // bubbleCurve: Curves.easeOutExpo,
-      // scaleCurve: Curves.easeOutExpo,
-      unSelectedColor: Color(0xff6c788a),
-      backgroundColor: Colors.white,
-      borderRadius: Radius.circular(17),
-      items: [
-        CustomNavigationBarItem(
-          icon: Icon(Icons.home_filled),
-        ),
-        CustomNavigationBarItem(
-          icon: Icon(Icons.access_time),
-        ),
-        CustomNavigationBarItem(
-          icon: Icon(Icons.settings),
-        ),
-      ],
-      currentIndex: _currentIndex,
-      onTap: (index) {
-        setState(() {
-          _currentIndex = index;
-          _tabController.index = _currentIndex;
-        });
-      },
+  Padding getIcon(int index, Color color) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 3),
+      child: Icon(widget.icons[index], size: 33, color: color),
     );
   }
 }
