8d7da0dec21a2f67834a8e8087835e2151a14bd8
Neil Goyal <neilgo1234@gmail.com>
2021-01-23 21:22:54 +0530

    m

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:motion_tab_bar/motiontabbar.dart';
+import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:schoolcalendar/pages/home.dart';
 import 'package:schoolcalendar/pages/settings.dart';
 import 'package:schoolcalendar/pages/timetable.dart';
@@ -7,66 +7,61 @@ import 'package:schoolcalendar/pages/timetable.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Custom Navigation Bar Example',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: MyHomePage(title: 'Example'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
+class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
-  TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(initialIndex: 0, vsync: this, length: 3);
-    _tabController.addListener(() {
-      setState(() {
-        _currentIndex = _tabController.index;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TabBarView(
-        controller: _tabController,
-        children: [
+      // extendBody for floating bar get better perfomance
+      body: PageView(
+        children: <Widget>[
           HomePage(),
           TimetablePage(),
           SettingsPage(),
         ],
       ),
-<<<<<<< HEAD
-      bottomNavigationBar: CustomNavigationBar(
+      extendBody: true,
+      backgroundColor: Colors.white,
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return _buildBorderRadiusDesign();
+  }
+
+  Widget _buildBorderRadiusDesign() {
+    return CustomNavigationBar(
       isFloating: false,
       scaleFactor: 0.3,
       iconSize: 30.0,
       elevation: 3,
       selectedColor: Colors.amber,
       strokeColor: Colors.white,
-      // bubbleCurve: Curves.easeOutExpo,
-      // scaleCurve: Curves.easeOutExpo,
       unSelectedColor: Color(0xff6c788a),
       backgroundColor: Colors.white,
       borderRadius: Radius.circular(17),
@@ -83,34 +78,10 @@ class _MyHomePageState extends State<MyHomePage>
       ],
       currentIndex: _currentIndex,
       onTap: (index) {
-=======
-      bottomNavigationBar: _buildBottomNavigationBar(),
-    );
-  }
-
-  Widget _buildBottomNavigationBar() {
-    return MotionTabBar(
-      labels: ["Home", "TimeTable", "Settings"],
-      initialSelectedTab: "Home",
-      tabIconColor: Colors.black,
-      tabSelectedColor: Color.fromRGBO(223, 164, 160, 1),
-      onTabItemSelected: (int value) {
->>>>>>> f2b31297ee602a2b6aab300061414c73d9f004ea
         setState(() {
-          _currentIndex = value;
-          _tabController.index = _currentIndex;
+          _currentIndex = index;
         });
       },
-<<<<<<< HEAD
-    )
-=======
-      icons: [Icons.home_filled, Icons.access_time, Icons.settings],
-      textStyle: TextStyle(color: Colors.black),
->>>>>>> f2b31297ee602a2b6aab300061414c73d9f004ea
     );
   }
-
- 
-
-  
 }
