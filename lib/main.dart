f2b31297ee602a2b6aab300061414c73d9f004ea
Neil Goyal <neilgo1234@gmail.com>
2021-01-23 20:06:54 +0530

    q

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:schoolcalendar/custom_navigation_bar.dart';
+import 'package:motion_tab_bar/motiontabbar.dart';
 import 'package:schoolcalendar/pages/home.dart';
 import 'package:schoolcalendar/pages/settings.dart';
 import 'package:schoolcalendar/pages/timetable.dart';
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage>
   @override
   void initState() {
     super.initState();
-    _tabController = new TabController(initialIndex: 0, vsync: this, length: 3);
+    _tabController = TabController(initialIndex: 0, vsync: this, length: 3);
     _tabController.addListener(() {
       setState(() {
         _currentIndex = _tabController.index;
@@ -62,40 +62,19 @@ class _MyHomePageState extends State<MyHomePage>
   }
 
   Widget _buildBottomNavigationBar() {
-    return _buildBorderRadiusDesign();
-  }
-
-  Widget _buildBorderRadiusDesign() {
-    return CustomNavigationBar(
-      isFloating: false,
-      scaleFactor: 0.3,
-      iconSize: 30.0,
-      elevation: 3,
-      selectedColor: Colors.amber,
-      strokeColor: Colors.white,
-      // bubbleCurve: Curves.easeOutExpo,
-      // scaleCurve: Curves.easeOutExpo,
-      unSelectedColor: Color(0xff6c788a),
-      backgroundColor: Colors.white,
-      borderRadius: Radius.circular(17),
-      items: [
-        CustomNavigationBarItem(
-          icon: Icon(Icons.home_filled),
-        ),
-        CustomNavigationBarItem(
-          icon: Icon(Icons.access_time),
-        ),
-        CustomNavigationBarItem(
-          icon: Icon(Icons.settings),
-        ),
-      ],
-      currentIndex: _currentIndex,
-      onTap: (index) {
+    return MotionTabBar(
+      labels: ["Home", "TimeTable", "Settings"],
+      initialSelectedTab: "Home",
+      tabIconColor: Colors.black,
+      tabSelectedColor: Color.fromRGBO(223, 164, 160, 1),
+      onTabItemSelected: (int value) {
         setState(() {
-          _currentIndex = index;
+          _currentIndex = value;
           _tabController.index = _currentIndex;
         });
       },
+      icons: [Icons.home_filled, Icons.access_time, Icons.settings],
+      textStyle: TextStyle(color: Colors.black),
     );
   }
 }
