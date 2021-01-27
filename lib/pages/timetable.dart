092a3103b8b4c426ee9edbb00812380f95fa0d37
Neil Goyal <neilgo1234@gmail.com>
2021-01-27 21:50:16 +0530

    days sorted

--- lib/pages/timetable.dart
+++ lib/pages/timetable.dart
@@ -65,7 +65,7 @@ class _TimetableState extends State<TimetablePage> {
                                   ]),
                               Container(
                                 child: FutureBuilder<DayDecrypted>(
-                                  // future: futureDay,
+                                  future: futureDay,
                                   builder: (context, snapshot) {
                                     if (snapshot.hasData) {
                                       var result =
