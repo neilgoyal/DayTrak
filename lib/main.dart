360985d477c169456f26ec4dda05810d8d6ba2f9
Neil Goyal <neilgo1234@gmail.com>
2021-02-01 22:42:06 +0530

    q

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:schoolcalendar/pages/home.dart';
 import 'package:schoolcalendar/firstopenpages/fp1.dart';
-import 'package:schoolcalendar/firstopenpages/fp2.dart';
-import 'package:schoolcalendar/firstopenpages/fp3.dart';
-import 'pages/home.dart';
-import 'pages/settings.dart';
-import 'pages/timetable.dart';
-import 'package:schoolcalendar/globals.dart' as globals;
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -21,131 +15,48 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: new Splash(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class Splash extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  SplashState createState() => new SplashState();
 }
 
-class CircleTabIndicator extends Decoration {
-  final BoxPainter _painter;
-
-  CircleTabIndicator({@required Color color, @required double radius})
-      : _painter = _CirclePainter(color, radius);
-
+class SplashState extends State<Splash> {
   @override
-  BoxPainter createBoxPainter([onChanged]) => _painter;
-}
-
-class _CirclePainter extends BoxPainter {
-  final Paint _paint;
-  final double radius;
-
-  _CirclePainter(Color color, this.radius)
-      : _paint = Paint()
-          ..color = color
-          ..isAntiAlias = true;
-
-  @override
-  void paint(Canvas canvas, Offset offset, ImageConfiguration cfg) {
-    final Offset circleOffset =
-        offset + Offset(cfg.size.width / 2, cfg.size.height - radius + 2);
-    canvas.drawCircle(circleOffset, radius, _paint);
+  void initState() {
+    super.initState();
+    new Timer(new Duration(milliseconds: 200), () {
+      checkFirstSeen();
+    });
   }
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<Widget> pages = [
-    HomePage(),
-    TimetablePage(),
-    SettingsPage(),
-    Fp1Page(),
-    Fp2Page(),
-    Fp3Page(),
-  ];
+  Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+
+    if (_seen) {
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => HomePage()));
+    } else {
+      prefs.setBool('seen', true);
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => Fp1Page()));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    globals.h1 = width * 0.115;
-    globals.h2 = width * 0.1;
-    globals.h3 = width * 0.09;
-    globals.h4 = width * 0.079;
-    globals.h5 = width * 0.069;
-    globals.h6 = width * 0.065;
-    globals.h7 = width * 0.06;
-    globals.h8 = width * 0.055;
-    globals.h9 = width * 0.05;
-    globals.h10 = width * 0.045;
-    globals.s1 = width * 0.3;
-    globals.s2 = width * 0.275;
-    globals.s3 = width * 0.25;
-    globals.s4 = width * 0.225;
-    globals.s5 = width * 0.13;
-    globals.w = width * 0.85;
-
-    return DefaultTabController(
-      length: 6,
-      initialIndex: 0,
-      child: Scaffold(
-        body: TabBarView(
-          children: pages,
-        ),
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: Color.fromRGBO(250, 250, 250, 1),
-          ),
-          margin: EdgeInsets.only(bottom: 15),
-          child: new TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(
-                  CupertinoIcons.house,
-                  size: 35,
-                ),
-              ),
-              Tab(
-                icon: Icon(
-                  CupertinoIcons.list_dash,
-                  size: 35,
-                ),
-              ),
-              Tab(
-                icon: Icon(
-                  CupertinoIcons.settings_solid,
-                  size: 35,
-                ),
-              ),
-              Tab(
-                icon: Icon(
-                  CupertinoIcons.phone,
-                  size: 35,
-                ),
-              ),
-              Tab(
-                icon: Icon(
-                  CupertinoIcons.phone,
-                  size: 35,
-                ),
-              ),
-              Tab(
-                icon: Icon(
-                  CupertinoIcons.phone,
-                  size: 35,
-                ),
-              ),
-            ],
-            unselectedLabelColor: Color.fromRGBO(150, 150, 150, 1),
-            indicatorSize: TabBarIndicatorSize.label,
-            labelColor: Colors.black,
-            indicator:
-                CircleTabIndicator(color: const Color(0xffbadfca), radius: 3.5),
-            enableFeedback: true,
-          ),
+    return new Scaffold(
+      body: new Center(
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            new Text('Loading...'),
+          ],
         ),
       ),
     );
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,114 @@
 import 'package:flutter/material.dart';
 import 'package:schoolcalendar/addtask1.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:schoolcalendar/firstopenpages/fp1.dart';
+import 'package:schoolcalendar/firstopenpages/fp2.dart';
+import 'settings.dart';
+import 'timetable.dart';
+import 'package:schoolcalendar/globals.dart' as globals;
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
+class CircleTabIndicator extends Decoration {
+  final BoxPainter _painter;
+
+  CircleTabIndicator({@required Color color, @required double radius})
+      : _painter = _CirclePainter(color, radius);
+
+  @override
+  BoxPainter createBoxPainter([onChanged]) => _painter;
+}
+
+class _CirclePainter extends BoxPainter {
+  final Paint _paint;
+  final double radius;
+
+  _CirclePainter(Color color, this.radius)
+      : _paint = Paint()
+          ..color = color
+          ..isAntiAlias = true;
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration cfg) {
+    final Offset circleOffset =
+        offset + Offset(cfg.size.width / 2, cfg.size.height - radius + 2);
+    canvas.drawCircle(circleOffset, radius, _paint);
+  }
+}
+
 class _HomePageState extends State<HomePage> {
+  List<Widget> pages = [
+    Addtask1(),
+    TimetablePage(),
+    SettingsPage(),
+    Fp1Page(),
+    Fp2Page(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Addtask1();
+    double width = MediaQuery.of(context).size.width;
+    globals.h1 = width * 0.115;
+    globals.h2 = width * 0.1;
+    globals.h3 = width * 0.09;
+    globals.h4 = width * 0.079;
+    globals.h5 = width * 0.069;
+    globals.h6 = width * 0.065;
+    globals.h7 = width * 0.06;
+    globals.h8 = width * 0.055;
+    globals.h9 = width * 0.05;
+    globals.h10 = width * 0.045;
+    globals.s1 = width * 0.3;
+    globals.s2 = width * 0.275;
+    globals.s3 = width * 0.25;
+    globals.s4 = width * 0.225;
+    globals.s5 = width * 0.13;
+    globals.w = width * 0.85;
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: Scaffold(
+        body: TabBarView(
+          children: pages,
+        ),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: Color.fromRGBO(250, 250, 250, 1),
+          ),
+          margin: EdgeInsets.only(bottom: 15),
+          child: new TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(
+                  CupertinoIcons.house,
+                  size: 35,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  CupertinoIcons.list_dash,
+                  size: 35,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  CupertinoIcons.settings_solid,
+                  size: 35,
+                ),
+              ),
+            ],
+            unselectedLabelColor: Color.fromRGBO(150, 150, 150, 1),
+            indicatorSize: TabBarIndicatorSize.label,
+            labelColor: Colors.black,
+            indicator:
+                CircleTabIndicator(color: const Color(0xffbadfca), radius: 3.5),
+            enableFeedback: true,
+          ),
+        ),
+      ),
+    );
   }
 }
