b0573b3341f9b3243f46d5bbc47e73dcd202a0ad
Vinayak Kapur <kapurvinayak2004@gmail.com>
2021-06-08 02:43:14 +0530

    homepage scrollable parts added

--- lib/pages/home2.dart
+++ lib/pages/home2.dart
@@ -11,50 +11,94 @@ class _Home2State extends State<Home2Page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: s6,
-        elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Column(
-                  children: [
-                    IconButton(
-                        onPressed: () {},
-                        padding:
-                            EdgeInsets.only(top: 0.0, left: 0.0, right: 0.0, bottom: 45,),
-                        icon: Icon(
-                          CupertinoIcons.person_alt_circle,
-                          size: s6,
+        appBar: AppBar(
+          toolbarHeight: s6,
+          elevation: 0,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          padding: EdgeInsets.only(
+                            top: 0.0,
+                            left: 0.0,
+                            right: 0.0,
+                            bottom: 45,
+                          ),
+                          icon: Icon(
+                            CupertinoIcons.person_alt_circle,
+                            size: s6,
+                          ))
+                    ],
+                  ),
+                  Column(children: [
+                    Text("  Good " "Morning",
+                        style: TextStyle(
+                          fontFamily: 'Protipo Compact',
+                          fontSize: h9,
+                          fontWeight: FontWeight.w300,
                         ))
-                  ],
-                ),
-                Column(children: [
-                  Text("  Good " "Morning",
+                  ]),
+                ],
+              ),
+              Column(
+                children: [
+                  Text("Day 6",
                       style: TextStyle(
                         fontFamily: 'Protipo Compact',
-                        fontSize: h9,
+                        fontSize: h4,
                         fontWeight: FontWeight.w300,
                       ))
-                ]),
-              ],
-            ),
-            Column(
-              children: [
-                Text("Day 6",
-                    style: TextStyle(
-                      fontFamily: 'Protipo Compact',
-                      fontSize: h4,
-                      fontWeight: FontWeight.w300,
-                    ))
-              ],
-            )
-          ],
+                ],
+              )
+            ],
+          ),
         ),
-      ),
-    );
+        body: ListView(children: [
+          SizedBox(
+            height: h1,
+          ),
+          Text("Upcoming Classes:",
+              style: TextStyle(
+                fontSize: h4,
+              )),
+              SizedBox(
+             height: h2,
+           ),
+          Card(
+            color: Colors.blue,
+            child: Text(
+              "hellp",
+              style: TextStyle(
+                fontSize: 80,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: h1,
+          ),
+          Text("Tasks:",
+              style: TextStyle(
+                fontSize: h4,
+              )),
+              SizedBox(
+             height: h2,
+           ),
+          Card(
+            color: Colors.blue,
+            child: Text(
+              "hellp",
+              style: TextStyle(
+                fontSize: 80,
+              ),
+            ),
+          ),
+          
+        ]));
   }
 }
