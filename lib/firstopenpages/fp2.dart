c35c7a97309f00d3e0957dde3eb3c3da1f920a5f
Neil Goyal <neilgo1234@gmail.com>
2021-07-01 00:56:55 +0530

    fp2

--- lib/firstopenpages/fp2.dart
+++ lib/firstopenpages/fp2.dart
@@ -4,7 +4,7 @@ import '../globals.dart' as globals;
 import 'package:flutter/rendering.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/services.dart';
-import '../pages/home.dart';
+import '../pages/home2.dart';
 
 String errmessage = "Cannot Be Empty";
 OutlineInputBorder textfieldborderenabled = OutlineInputBorder(
@@ -738,7 +738,7 @@ class _Fp2State extends State<Fp2Page> {
                                 prefs.setString('B6', myController6.text);
                                 Navigator.of(context).pushReplacement(
                                     new MaterialPageRoute(
-                                        builder: (context) => HomePage()));
+                                        builder: (context) => Home2Page()));
                               }
                             } else if (globals.valueOfGrade == 91 ||
                                 globals.valueOfGrade == 92) {
@@ -756,7 +756,7 @@ class _Fp2State extends State<Fp2Page> {
                                 prefs.setString('B9', myController9.text);
                                 Navigator.of(context).pushReplacement(
                                     new MaterialPageRoute(
-                                        builder: (context) => HomePage()));
+                                        builder: (context) => Home2Page()));
                               }
                             }
                           },
